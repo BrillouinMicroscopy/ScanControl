c141f9c7c78706394c7777c75c7487b2931a59d4
Raimund Schlüßler <raimund.schluessler@tu-dresden.de>
2018-07-04 10:59:48 +0200

    Update CANControl, remove NewportXPS, move to Github

--- ScanControl.m
+++ ScanControl.m
@@ -13,20 +13,21 @@ classdef ScanControl < handle
             if strcmp(device,'LSM510')
                 obj.device = Utils.ScanControl.LSM510Control();
             elseif strcmp(device,'XPS')
-                connection.IP = '192.168.0.254';    % IP Adress of the Controller
-                connection.Port = 5001;             % Port number of the Controller
-                connection.TimeOut = 60;            % Timeout to use
+                disp('Device not implemented.');
+                % connection.IP = '192.168.0.254';    % IP Adress of the Controller
+                % connection.Port = 5001;             % Port number of the Controller
+                % connection.TimeOut = 60;            % Timeout to use
                 
-                settings.velocity = 1;              % [mm/s]    velocity of the stage
-                settings.acceleration = 1;          % [mm/s^2]  acceleration of the stage
+                % settings.velocity = 1;              % [mm/s]    velocity of the stage
+                % settings.acceleration = 1;          % [mm/s^2]  acceleration of the stage
                 
-                % Connect to the controller via TCP
-                obj.device = XPS(connection.IP, connection.Port, connection.TimeOut);
-                % Initialize the home position of the stages
-                obj.device.init();
+                % % Connect to the controller via TCP
+                % obj.device = XPS(connection.IP, connection.Port, connection.TimeOut);
+                % % Initialize the home position of the stages
+                % obj.device.init();
                 
-                % set parameters of the translation
-                obj.device.set(settings);
+                % % set parameters of the translation
+                % obj.device.set(settings);
             end
         end
         
